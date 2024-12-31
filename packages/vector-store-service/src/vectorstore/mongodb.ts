4f1e9816285362e2df1977262136f8a39acd74a5
dingyi <dingyi222666@foxmail.com>
2024-12-31 20:24:20 +0800

    feat(vector-store): integrate MongoDB support and update vector store middleware

--- packages/vector-store-service/src/vectorstore/mongodb.ts
+++ packages/vector-store-service/src/vectorstore/mongodb.ts
@@ -1,10 +1,8 @@
-import { MongoDBAtlasVectorSearch } from '@langchain/mongodb'
 import { Context, Logger } from 'koishi'
 import { ChatLunaPlugin } from 'koishi-plugin-chatluna/services/chat'
 import { createLogger } from 'koishi-plugin-chatluna/utils/logger'
 import { Config } from '..'
 import { ChatLunaSaveableVectorStore } from 'koishi-plugin-chatluna/llm-core/model/base'
-import { MongoClient, ObjectId } from 'mongodb'
 
 let logger: Logger
 
@@ -19,12 +17,13 @@ export async function apply(
         return
     }
 
-    await importMongoDB()
+    const MongoDB = await importMongoDB()
+    const MongoDBAtlas = await importMongoDBAtlasVectorSearch()
 
     plugin.registerVectorStore('mongodb', async (params) => {
         const embeddings = params.embeddings
 
-        const client = new MongoClient(config.mongodbUrl)
+        const client = new MongoDB.MongoClient(config.mongodbUrl)
         await client.connect()
 
         ctx.on('dispose', async () => {
@@ -36,71 +35,70 @@ export async function apply(
             .db(config.mongodbDbName)
             .collection(config.mongodbCollectionName)
 
-        const vectorStore = new MongoDBAtlasVectorSearch(embeddings, {
-            collection,
-            indexName: params.key ?? 'vector_index',
-            textKey: 'text',
-            embeddingKey: 'embedding'
-        })
+        const vectorStore = new MongoDBAtlas.MongoDBAtlasVectorSearch(
+            embeddings,
+            {
+                collection,
+                indexName: params.key ?? 'vector_index',
+                textKey: 'text',
+                embeddingKey: 'embedding'
+            }
+        )
 
-        const wrapperStore =
-            new ChatLunaSaveableVectorStore<MongoDBAtlasVectorSearch>(
-                vectorStore,
-                {
-                    async deletableFunction(_store, options) {
-                        if (options.deleteAll) {
-                            await collection.deleteMany({})
-                            return
-                        }
-
-                        const ids: string[] = []
-                        if (options.ids) {
-                            ids.push(...options.ids)
-                        }
-
-                        if (options.documents) {
-                            const documentIds = options.documents
-                                ?.map(
-                                    (document) =>
-                                        document.metadata?.raw_id as
-                                            | string
-                                            | undefined
-                                )
-                                .filter((id): id is string => id != null)
-
-                            ids.push(...documentIds)
-                        }
-
-                        if (ids.length > 0) {
-                            await collection.deleteMany({
-                                _id: { $in: ids.map((id) => new ObjectId(id)) }
-                            })
-                        }
-                    },
-                    async addDocumentsFunction(
-                        store,
-                        documents,
-                        options: { ids?: string[] }
-                    ) {
-                        let keys = options?.ids ?? []
-
-                        keys = documents.map((document, i) => {
-                            const id = keys[i] ?? crypto.randomUUID()
-                            document.metadata = {
-                                ...document.metadata,
-                                raw_id: id
-                            }
-                            return id
-                        })
-
-                        await store.addDocuments(documents)
-                    },
-                    async saveableFunction(_store) {
-                        await client.close()
-                        logger.info('MongoDB connection closed during save')
-                    }
+        const wrapperStore = new ChatLunaSaveableVectorStore<
+            typeof vectorStore
+        >(vectorStore, {
+            async deletableFunction(_store, options) {
+                if (options.deleteAll) {
+                    await collection.deleteMany({})
+                    return
                 }
-            )
+
+                const ids: string[] = []
+                if (options.ids) {
+                    ids.push(...options.ids)
+                }
+
+                if (options.documents) {
+                    const documentIds = options.documents
+                        ?.map(
+                            (document) =>
+                                document.metadata?.raw_id as string | undefined
+                        )
+                        .filter((id): id is string => id != null)
+
+                    ids.push(...documentIds)
+                }
+
+                if (ids.length > 0) {
+                    await collection.deleteMany({
+                        _id: { $in: ids.map((id) => new MongoDB.ObjectId(id)) }
+                    })
+                }
+            },
+            async addDocumentsFunction(
+                store,
+                documents,
+                options: { ids?: string[] }
+            ) {
+                let keys = options?.ids ?? []
+
+                keys = documents.map((document, i) => {
+                    const id = keys[i] ?? crypto.randomUUID()
+                    document.metadata = {
+                        ...document.metadata,
+                        raw_id: id
+                    }
+                    return id
+                })
+
+                await store.addDocuments(documents)
+            },
+            async saveableFunction(_store) {
+                /* await client.close()
+                logger.info('MongoDB connection closed during save') */
+            }
+        })
 
         return wrapperStore
     })
@@ -108,12 +106,22 @@ export async function apply(
 
 async function importMongoDB() {
     try {
-        const { MongoClient } = await import('mongodb')
-        return { MongoClient }
+        return await import('mongodb')
     } catch (err) {
         logger.error(err)
         throw new Error(
             'Please install mongodb as a dependency with, e.g. `npm install -S mongodb`'
+        )
+    }
+}
+
+async function importMongoDBAtlasVectorSearch() {
+    try {
+        return await import('@langchain/mongodb')
+    } catch (err) {
+        logger.error(err)
+        throw new Error(
+            'Please install mongodb-atlas-vector-search as a dependency with, e.g. `npm install -S mongodb-atlas-vector-search`'
         )
     }
 }
