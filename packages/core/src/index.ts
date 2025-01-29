36ddc5dc5325c402eba34dfcffae16b2a73bc599
dingyi <dingyi222666@foxmail.com>
2025-01-30 02:33:00 +0800

    docs: update usage documentation for ChatLuna v1.0

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -36,13 +36,11 @@ export const inject2 = {
 export let logger: Logger
 
 export const usage = `
-## chatluna v1.0 rc
-
-### 目前插件在 rc 阶段，已经趋于稳定，但是还会有少量 bug，可以去插件主页那边提 issue 或加群反馈。
+## chatluna v1.0
 
 ChatLuna 插件交流群：282381753 （有问题或出现 Bug 先加群问）
 
-群里目前没有搭载该插件的 bot，加群的话最好是来询问问题或者提出意见的
+群里目前没有搭载该插件的 bot，加群的话最好是来询问问题或者提出意见的。
 
 [文档](https://chatluna.chat) 也在制作中，有问题可以在群里提出。
 `
